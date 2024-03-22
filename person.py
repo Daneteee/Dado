02f523ab396042145a9856dc158932adca5418c1
jerry02-daw <149429069+jerry02-daw@users.noreply.github.com>
2024-03-22 15:43:55 +0100

    Add files via upload

---
+++ person.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+
+class Person:
+    def __init__(self, name, last_name, date_born, phone):
+        self.name = name
+        self.last_name = last_name
+        self.date_born = date_born
+        self.phone = phone
+
+    def __str__(self):
+        return f"{self.name}, {self.last_name}, {self.date_born}"
+
+    def age_usu(self):
+        date_now = datetime.now()
+        data_usu = datetime.strptime(self.date_born, "%d-%m-%Y")
+        date_now_date = date_now.date()
+        operation = date_now_date - data_usu.date()
+        total_years = round(operation.days / 365)
+        print(f"Age in years to {self.name}, {self.last_name}: {total_years}")
+        return total_years
+
+    def is_bigger(self):
+        if self.age_usu() >= 18:
+            return True
+        else:
+            return False
